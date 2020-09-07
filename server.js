b823d3a8d576ebdd3fa15a26377e5b0f088f29ec
Eliana Simas <elisimas@gmail.com>
2020-09-07 20:27:26 -0300

    Update server.js

--- server.js
+++ server.js
@@ -12,6 +12,26 @@ function calcularAreaPerimetro(lado) {
     return perimetro;
 }
 
+function calcularPerimetroPor(figuraPlana) {
+    console.log(figuraPlana);
+    switch (figuraPlana.objeto.toUpperCase()) {
+        case 'TRIANGULO':
+            console.log(figuraPlana);
+            console.log(lados);
+
+            break;
+        case 'RETANGULO':
+
+            break;
+        case 'QUADRADO':
+
+            break;
+        default:
+            break;
+    }
+
+}
+
 const server = restify.createServer({
     name: 'Server Node',
     version: '1.0.0'
@@ -21,8 +41,15 @@ server.use(restify.plugins.acceptParser(server.acceptable));
 server.use(restify.plugins.queryParser());
 server.use(restify.plugins.bodyParser());
 
-server.post('/perimetro/:lado', function(req, res, next) {
-    res.send(req.params);
+server.post('/perimetro', function(req, res, next) {
+    //res.send(req.params);
+    // var calculaperimetro = parseInt(req.body.lado) * 4;
+    //res.send({ lado: parseInt(req.body.lado), resultado: calculaperimetro });
+    var corpo = [];
+    corpo = req.body;
+    //console.log(corpo[1]);
+    calcularPerimetroPor(corpo[0].objeto, corpo[0].lados);
+    res.send(corpo);
     return next();
 });
 
